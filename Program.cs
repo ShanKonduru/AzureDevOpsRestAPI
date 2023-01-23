f2e3d1240861106fbced217e6e515d6fb2111fff
Shan Konduru <ShanKonduru@users.noreply.github.com>
2023-01-23 21:59:08 +0530

    code updated

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+QueryExecutor qry = new QueryExecutor("WinstonBrand", "eex2tngmzd3vdqzo2twt46akt5lplknjbjsvqd6i65cpf4sbrviq");
+
+Task<IList<WorkItem>>  openBugs = qry.QueryOpenBugs("Phase-ONE");
